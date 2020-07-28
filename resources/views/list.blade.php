2655c133dab94a04a5840b1b0fdbb113e7d785be
luciusis <luciusis@outlook.com>
2020-07-28 17:57:31 +0800

    1

---
+++ resources/views/list.blade.php
@@ -0,0 +1,46 @@
+﻿<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>笔记</title>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!--[if lt IE 9]>
+      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+	<div class="container" style="margin-top: 60px;">
+		<div class="text-center"><h2>读书笔记</h2></div>
+		<style>
+			.digest_count{
+				float:right;
+				width:70px;
+			}
+		</style>
+		<div class="col-sm-12">
+			<div class="list-group">
+				<a href="detail.html" class="list-group-item">挪威的森林<span class="digest_count">笔记：300</span></a>
+				<a href="detail.html" class="list-group-item">三生三世十里桃花<span class="digest_count">笔记：40</span></a>
+				<a href="detail.html" class="list-group-item">从你的全世界路过<span class="digest_count">笔记：68</span></a>
+				<a href="detail.html" class="list-group-item">解忧杂货铺<span class="digest_count">笔记：760</span></a>
+				<a href="detail.html" class="list-group-item">乖，摸摸头<span class="digest_count">笔记：500</span></a>
+				<a href="detail.html" class="list-group-item">小王子<span class="digest_count">笔记：900</span></a>
+				<a href="detail.html" class="list-group-item">古文观止<span class="digest_count">笔记：40</span></a>
+				<a href="detail.html" class="list-group-item">朦胧诗<span class="digest_count">笔记：21</span></a>
+				<a href="detail.html" class="list-group-item">把时间当做朋友<span class="digest_count">笔记：10</span></a>
+				<a href="detail.html" class="list-group-item">白夜行<span class="digest_count">笔记：7</span></a>
+			</div>
+		</div>
+		<div class="col-sm-12 text-right">
+			<a href="book_add.html" class="btn btn-success">添加</a>
+		</div> 
+	</div>
+    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
