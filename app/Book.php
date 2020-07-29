d01ae802b04514ac5f1987fb419ed6df2b93947d
luciusis <luciusis@outlook.com>
2020-07-29 18:00:24 +0800

    11

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,34 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Book
+ *
+ * @property int $id
+ * @property string $name 书名
+ * @property int $sort 排序
+ * @property int $status 状态 1正常 2屏蔽
+ * @property string $author 作者
+ * @property string $user 添加用户
+ * @property int $clicks 点击量
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Digest[] $digests
+ * @property-read int|null $digests_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereClicks($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereSort($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereUser($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     //
--- app/Digest.php
+++ app/Digest.php
@@ -4,6 +4,27 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Digest
+ *
+ * @property int $id
+ * @property int $book_id 所属书目ID
+ * @property string $content 书摘内容
+ * @property int $status 状态 1正常 2屏蔽
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Book $book
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Digest whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Digest extends Model
 {
     //
